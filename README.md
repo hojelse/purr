916446f2d29aa5fc408989f688e93ca44e773ac6
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-10-20 17:27:00 +0200

    Add plans and stuff

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # purr
 A Kattis CLI
+
+## login
+
+`purr login`
+
+```
+> log in to open.kattis.com
+> enter another host...
+```
+
+download kattisrc in background (no file dragging)
+
+## push
+
+`purr push <open/itu/all> <problemid>`
+
+## test
+
+`purr test <problemid>`
+`purr watch <problemid>`
---
+++ problems/proofs/proofs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class compass
+{
+  static void Main(string[] args) {
+    int N = int.Parse(Console.ReadLine());
+
+    HashSet<string> set = new HashSet<string>();
+
+    for (int i = 0; i < N; i++)
+    {
+      List<string> tokens = Console.ReadLine().Split(" ").ToList();
+      if (tokens.Count() == 2)
+      {
+        set.Add(tokens[1]);
+      } else {
+        var right = tokens.Last();
+
+        for (int j = 0; j < tokens.Count()-2; j++)
+        {
+          // Console.WriteLine($"debug: {tokens[j]}");
+          if (!set.Contains(tokens[j]))
+          {
+            Console.WriteLine(i+1);
+            return;
+          }
+        }
+        set.Add(right);
+      }
+    }
+
+    Console.WriteLine("correct");
+  }
+}
